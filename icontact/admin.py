39e3454c4aca58967c52b2a428bbc9d4036d9b6b
Caroline Simpson <csimpson@cigionline.org>
2015-04-16 19:19:15 -0400

    Adding inline message clicks to message admin.

--- icontact/admin.py
+++ icontact/admin.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from .models import Account, Contact, Action, MessageClick, \
     Message, List, Campaign, Send, Offset, Subscription
 
+
+class ClickInline(admin.TabularInline):
+    model = MessageClick
+    extra = 1
+
+
+class MessageAdmin(admin.ModelAdmin):
+    inlines = (ClickInline, )
+
+
 admin.site.register(Account)
 admin.site.register(Contact)
 admin.site.register(Action)
 admin.site.register(List)
 admin.site.register(Campaign)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(MessageClick)
 admin.site.register(Send)
 admin.site.register(Offset)
